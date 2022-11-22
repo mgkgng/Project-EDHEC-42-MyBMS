1a4902026206bd1ec13d7f5148d93324a871d0a0
Iman <iman-104@hotmail.fr>
2022-11-23 00:46:15 +0100

    forum topics added

--- MyBMS/src/lib/store.js
+++ MyBMS/src/lib/store.js
@@ -24,15 +24,31 @@ export const PatientList = [
 export const PharmaciesInCity = new Map();
 PharmaciesInCity.set("06000", [
 	{ name: "Pharmacie Riviera 24h24 (de garde)", address: "66 av. Jean Médecin 06000 Nice"},
+	{ name: "Pharmacie Gambetta", address: "34 bd Gambetta 06000 Nice"},
+	{ name: "Pharmacie de France", address: "13 bd Gambetta 06000 Nice"},
+	{ name: "Pharmacie Lyautey", address: "33 av du Maréchal Lyautey 06000 Nice"},
 ])
 
+PharmaciesInCity.set("06400", [
+	{ name: "Pharmacie de la Californie", address: "137 rue d'Antibes 06400 Cannes"},
+	{ name: "Pharmacie du Casino", address: "9 sq Mérimée 06400 Cannes"},
+	{ name: "Pharmacie du Bocage", address: "26 av des Coteaux 06400 Cannes"},
+	{ name: "Pharmacie du Pont Carnot", address: "2 bd Carnot 06400 Cannes"},
+])
 
 export const Specialities = ["oncology", "cardiology", "immunology", "hematology", "fibrosis"]
 
 export const ForumTopics = [
-	{title: "", author: "", date: "", like: 254, dislike: 12, commentNb: 6},
-	{title: ""},
-	{},
+	{title: "Somnolence après prise du traitement le matin", author: "Marshall P.", date: "12/11/2022", like: 254, commentNb: 6, visitNb: 2514},
+	{title: "Convalescence après embolie pulmonaire", author: "Hubert T.", date: "14/10/2022", like: 687, commentNb: 37, visitNb: 3425},
+	{title: "Eviter l'exposition au soleil pendant le traitement en été", author: "Florine F.", date: "22/11/2022", like: 78, commentNb: 4, visitNb: 1732},
+	{title: "Palpitations fortes même au repos", author: "Salim D.", date: "21/11/2022", like: 12, commentNb: 1, visitNb: 47},
+	{title: "Besoin de conseils après pose de pacemaker", author: "Daniela R.", date: "15/11/2022", like: 65, commentNb: 10, visitNb: 537},
+	{title: "Douleurs post ponction lombaire", author: "Robert N.", date: "7/11/2022", like: 465, commentNb: 54, visitNb: 2737},
+	{title: "[Conseils] Quelle alimentation pour réduire le cholestérol", author: "Rumi W.", date: "19/11/2022", like: 132, commentNb: 6, visitNb: 437},
+	{title: "Compatibilité avec un traitement anxiolytique ?", author: "Tristan F.", date: "20/11/2022", like: 57, commentNb: 13, visitNb: 256},
+	{title: "Mon apnée du sommeil a disparu", author: "Gontran A.", date: "1/11/2022", like: 210, commentNb: 43, visitNb: 88},
+	{title: "Reprendre une activité sportive après un traitement", author: "Marin G.", date: "5/11/2022", like: 32, commentNb: 3, visitNb: 132},
 ];
 
 export const userType = writable(UserType.Unknown);
