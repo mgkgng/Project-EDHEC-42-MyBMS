f0a47ad0ac4591ad7190cf3b06464b203963da73
Minguk Gaang <minguk.gaang@gmail.com>
2022-11-23 00:51:54 +0100

    crabi icon

--- MyBMS/src/routes/Footer.svelte
+++ MyBMS/src/routes/Footer.svelte
@@ -1,39 +1,46 @@
-<style lang="scss">
-	footer {
-		position: absolute;
-		bottom: 0;
-		width: 100%;
-		height: 50px;
-		background-color: #fff;
+	<style lang="scss">
+		footer {
+			position: absolute;
+			bottom: 0;
+			width: 100%;
+			height: 50px;
+			background-color: #fff;
 
-		display: flex;
-		flex-direction: column;
-		justify-content: space-around;
-		user-select: none;
+			display: flex;
+			flex-direction: column;
+			justify-content: space-around;
+			user-select: none;
 
-		.lang {
-			padding-left: .8em;
+			.lang {
+				padding-left: .8em;
 
-			.choice { cursor: pointer; }
-			.selected {
-				padding-bottom: .1em;
-				border-bottom: 1px solid #6c6c6c;
+				.choice { cursor: pointer; }
+				.selected {
+					padding-bottom: .1em;
+					border-bottom: 1px solid #6c6c6c;
+				}
+			}
+			img {
+				position: absolute;
+				width: 180px;
+				bottom: 0;
+				right: 0;
 			}
 		}
-	}
-</style>
+	</style>
 
-<script>
-	let langSet = ["English", "Français", "Deutsch", "Español", "Italiano"];
-	let langSelected = "Français";
-</script>
+	<script>
+		let langSet = ["English", "Français", "Deutsch", "Español", "Italiano"];
+		let langSelected = "Français";
+	</script>
 
-<footer>
-	<div class="flex lang">
-		{#each langSet as lang}
-		<div class="{(lang != langSelected) ? "choice" : "selected"}" on:click={() => {
-			langSelected = lang;
-		}}>{lang}</div>
-		{/each}
-	</div>
-</footer>
+	<footer>
+		<div class="flex lang">
+			{#each langSet as lang}
+			<div class="{(lang != langSelected) ? "choice" : "selected"}" on:click={() => {
+				langSelected = lang;
+			}}>{lang}</div>
+			{/each}
+		</div>
+		<img src="crabi2.png" alt="crab">
+	</footer>
